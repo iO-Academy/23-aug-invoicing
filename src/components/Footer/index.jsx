9e6b92188d1fcb224e3744881fa4f401edbac1c7
Claudia Lim <climweiching@gmail.com>
2023-10-23 16:54:13 +0100

    bootstrap fine-tuning

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className='d-none d-sm-block border-top m-3 p-2'>
+        <footer className='d-none d-sm-block border-top m-4 p-2'>
             <p className='small'>ⓒ Copyright iO Academy 2023</p>
         </footer>
     )
--- src/components/Toolbar/index.jsx
+++ src/components/Toolbar/index.jsx
@@ -1,6 +1,6 @@
 const Toolbar = () => {
     return (
-        <div className='row'>
+        <div className='row my-4 my-sm-0 mx-0'>
             <div className='col d-flex justify-content-start p-0'>
                 <button className='btn dropdown-toggle btn-sm'>Sort by</button>
                 <button className='btn dropdown-toggle btn-sm'>Filter by Status</button>
