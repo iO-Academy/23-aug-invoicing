7302745dabadf1c2050b76b73d82de9cf7c1d72e
Jake <jake_lewis12@hotmail.co.uk>
2023-10-24 09:44:41 +0100

    added formatting for t4

--- src/components/Invoice.jsx
+++ src/components/Invoice.jsx
@@ -4,13 +4,27 @@ import {useState, useEffect} from "react";
 const Invoice = ({invoice}) => {
     const {id, invoice_id, name, due, invoice_total, status, status_name} = invoice;
 
+    function statusClass() {
+        if (status_name === 'Pending') {
+            return 'btn-outline-warning';
+        } else if (status_name === 'Cancelled') {
+            return 'btn-outline-secondary';
+        } else if (status_name === 'Paid') {
+            return 'btn-outline-success';
+        } else {
+            return 'btn-outline-danger';
+        }
+    }
+
+    let formattedDate = new Date(due).toLocaleDateString('en-gb', {day:"numeric", month:"long", year:"numeric"})
+
     return (
-        <div className="border m-2 p-1 d-flex flex-row flex-wrap gap-3 justify-content-between">
-            <p className="fw-bold">{invoice_id}</p>
-            <p>{due}</p>
-            <p>{name}</p>
-            <p className="fw-bold w-50">{"£" + invoice_total}</p>
-            <p className="btn"><li>{status_name}</li></p>
+        <div className="border m-2 p-2 d-flex flex-row flex-wrap justify-content-between">
+            <p className="fw-bold m-0 small">#{invoice_id}</p>
+            <p className="m-0 small text-secondary">Due {formattedDate}</p>
+            <p className="m-0 small text-secondary fw-semibold">{name}</p>
+            <p className="fw-bold w-50 m-0">{"£ " + invoice_total}</p>
+            <p className={"btn m-0 p-1 " + statusClass()}><li className="small">{status_name}</li></p>
             <div className="d-none d-lg-block">></div>
         </div>
     )
--- src/components/InvoiceContainer.jsx
+++ src/components/InvoiceContainer.jsx
@@ -1,14 +1,14 @@
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 import Invoice from "./Invoice.jsx";
 
 const InvoiceContainer = (props) => {
-    return (
-        <div className="container">
-            {props.invoices.map((invoice) => {
-                return <Invoice key={invoice.id} invoice={invoice} />
-            })}
-        </div>
-    )
-}
+        return (
+            <div className="container">
+                {props.invoices.map((invoice) => {
+                    return <Invoice key={invoice.id} invoice={invoice} />
+                })}
+            </div>
+        )
+    }
 
-export default InvoiceContainer
+    export default InvoiceContainer
