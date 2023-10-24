76ea65abb496a6abf13cc97572778150b5ddb153
odics <odics@vcu.edu>
2023-10-24 09:28:33 +0100

    Add new plus icon

--- src/components/Toolbar/index.jsx
+++ src/components/Toolbar/index.jsx
@@ -1,16 +1,20 @@
-const Toolbar = () => {
-    return (
-        <div className='row my-4 my-sm-0 mx-0'>
-            <div className='col d-flex justify-content-start p-0'>
-                <button className='btn dropdown-toggle btn-sm'>Sort by</button>
-                <button className='btn dropdown-toggle btn-sm'>Filter by Status</button>
-            </div>
-            <div className='col d-flex justify-content-end p-0'>
-                <button className="btn btn-info text-white d-inline btn-sm"><i className="fa-solid fa-square-plus text-dark"></i> New Invoice</button>
-            </div>
-        </div>
-    )
+import PlusIcon from "../ui/PlusIcon";
 
-}
+const Toolbar = () => {
+  return (
+    <div className="row my-4 my-sm-0 mx-0">
+      <div className="col d-flex justify-content-start p-0">
+        <button className="btn dropdown-toggle btn-sm">Sort by</button>
+        <button className="btn dropdown-toggle btn-sm">Filter by Status</button>
+      </div>
+      <div className="col d-flex justify-content-end p-0">
+        <button className="btn btn-info text-white d-inline btn-sm d-flex align-items-center">
+          <PlusIcon />
+          New Invoice
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Toolbar;
---
+++ src/components/ui/PlusIcon.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../index.css";
+
+const PlusIcon = () => {
+  return (
+    <div className="plus-icon">
+      <i class="fa-solid fa-plus"></i>
+    </div>
+  );
+};
+
+export default PlusIcon;
