45928881a997fbf48c0e99b8ae3928ca6f9f8790
Mitch <michaelthierytucker@gmail.com>
2023-10-24 14:34:55 +0100

    final table date

--- src/components/Invoice.jsx
+++ src/components/Invoice.jsx
@@ -1,77 +1,77 @@
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import "../index.css";
 import InvoiceTable from "./InvoiceTable/index.jsx";
 
-const Invoice = ({ invoice }) => {
-  const { id, invoice_id, name, due, invoice_total, status, details, status_name } =
-    invoice;
+const Invoice = ({invoice}) => {
+    const {id, invoice_id, name, due, invoice_total, status, details, status_name} =
+        invoice;
 
-  const [show, setShow] = useState(false);
-  const [idForModal, setIdForModal] = useState("");
+    const [show, setShow] = useState(false);
+    const [idForModal, setIdForModal] = useState("");
 
-  const handleClose = () => setShow(false);
-  const handleShow = (id) => {
-    setIdForModal(id);
-    setShow(true);
-  };
+    const handleClose = () => setShow(false);
+    const handleShow = (id) => {
+        setIdForModal(id);
+        setShow(true);
+    };
 
-  function statusClass() {
-    if (status_name === "Pending") {
-      return "btn-outline-warning";
-    } else if (status_name === "Cancelled") {
-      return "btn-outline-secondary";
-    } else if (status_name === "Paid") {
-      return "btn-outline-success";
-    } else {
-      return "btn-outline-danger";
+    function statusClass() {
+        if (status_name === "Pending") {
+            return "btn-outline-warning";
+        } else if (status_name === "Cancelled") {
+            return "btn-outline-secondary";
+        } else if (status_name === "Paid") {
+            return "btn-outline-success";
+        } else {
+            return "btn-outline-danger";
+        }
     }
-  }
 
-  let formattedDate = new Date(due).toLocaleDateString("en-gb", {
-    day: "numeric",
-    month: "short",
-    year: "numeric",
-  });
+    let formattedDate = new Date(due).toLocaleDateString("en-gb", {
+        day: "numeric",
+        month: "short",
+        year: "numeric",
+    });
 
-  return (
-    <>
-      <div
-        className="border m-2 p-2 d-flex flex-row flex-wrap justify-content-between invoice-click"
-        onClick={() => {
-          handleShow(invoice_id);
-        }}
-      >
-        <p className="fw-bold m-0 small">#{invoice_id}</p>
-        <p className="m-0 small text-secondary">Due {formattedDate}</p>
-        <p className="m-0 small text-secondary fw-semibold">{name}</p>
-        <p className="fw-bold w-50 m-0">{"£ " + invoice_total}</p>
-        <div className="d-flex align-items-center gap-3">
-          <p
-            className={
-              "btn m-0 p-1 d-flex align-items-center gap-2 align-self-end " +
-              statusClass()
-            }
-          >
-            <i class="fa-solid fa-circle bullet"></i>
-            {status_name}
-          </p>
-          <i class="fa-solid fa-chevron-right d-none d-lg-block"></i>
-        </div>
-      </div>
-      <Modal show={show} onHide={handleClose} centered size="lg">
-        <Modal.Header closeButton>
-          <Modal.Title>Invoice #{idForModal}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <InvoiceTable invoice={invoice}/>
+    return (
+        <>
+            <div
+                className="border m-2 p-2 d-flex flex-row flex-wrap justify-content-between invoice-click"
+                onClick={() => {
+                    handleShow(invoice_id);
+                }}
+            >
+                <p className="fw-bold m-0 small">#{invoice_id}</p>
+                <p className="m-0 small text-secondary">Due {formattedDate}</p>
+                <p className="m-0 small text-secondary fw-semibold">{name}</p>
+                <p className="fw-bold w-50 m-0">{"£ " + invoice_total}</p>
+                <div className="d-flex align-items-center gap-3">
+                    <p
+                        className={
+                            "btn m-0 p-1 d-flex align-items-center gap-2 align-self-end " +
+                            statusClass()
+                        }
+                    >
+                        <i class="fa-solid fa-circle bullet"></i>
+                        {status_name}
+                    </p>
+                    <i class="fa-solid fa-chevron-right d-none d-lg-block"></i>
+                </div>
+            </div>
+            <Modal show={show} onHide={handleClose} centered size="lg">
+                <Modal.Header closeButton>
+                    <Modal.Title>Invoice #{idForModal}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <InvoiceTable invoice={invoice}/>
 
-        </Modal.Body>
-        <Modal.Footer></Modal.Footer>
-      </Modal>
-    </>
-  );
+                </Modal.Body>
+                <Modal.Footer></Modal.Footer>
+            </Modal>
+        </>
+    );
 };
 
 export default Invoice;
