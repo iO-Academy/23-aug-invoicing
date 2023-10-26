8aeb5176d7b0a0ad4cc3a594438137b531df6e71
odics <odics@vcu.edu>
2023-10-26 09:37:51 +0100

    Fixed longstanding bug

--- src/components/NewItemForm/index.jsx
+++ src/components/NewItemForm/index.jsx
@@ -1,39 +1,51 @@
-import {useState, useEffect} from "react";
-import React from 'react'
+import { useState, useEffect } from "react";
+import React from "react";
 import CreateNewInvoiceButton from "../CreateNewInvoiceButton/index.jsx";
 import NewItemRow from "../NewItemRow/";
 
 function NewItemForm() {
-    const [itemIndex, setItemIndex] = useState([1]);
-    const [total, setTotal] = useState(0);
-    const [invoiceObj, setInvoiceObj] = useState([{description:'', quantity:0, rate:0, total:0}])
-
-    return (
-        <>
-        <div>
-            <div className="d-flex flex-row w-100 small gap-1 fw-bold align-items-center py-1 border-top border-bottom my-1">
-                <div className="col-3 align-items-center">Description</div>
-                <div className="col-2 align-items-center">Quantity</div>
-                <div className="col-3 align-items-center">Rate</div>
-                <div className="col-2 align-items-center">Total</div>
-            </div>
-            <form method="post" className="d-flex flex-column justify-content-evenly">
-                {itemIndex.map((item, index) => {
-                    return (
-                <NewItemRow key={index} index={index} itemIndex= {itemIndex} setItemIndex= {setItemIndex} invoiceObj={invoiceObj} setInvoiceObj={setInvoiceObj}/>
-                )
-                })}
-            </form>
-            <div className="d-flex flex-row justify-content-end bg-warning fw-bold">
-                <span className="d-flex col-10 justify-content-end">Total</span>
-                <span className="col-2 d-flex justify-content-end">£0.00</span>
-            </div>
-            </div>
-            <div>
-                {/*<CreateNewInvoiceButton quantity={quantity} rate={rate} description={description} total={total}/>*/}
-            </div>
-        </>
-    )
+  const [itemIndex, setItemIndex] = useState([1]);
+  const [total, setTotal] = useState(0);
+  const [invoiceObj, setInvoiceObj] = useState([
+    { description: "", quantity: 0, rate: 0, total: 0 },
+  ]);
+  console.log(invoiceObj);
+  return (
+    <>
+      <div>
+        <div className="d-flex flex-row w-100 small gap-1 fw-bold align-items-center py-1 border-top border-bottom my-1">
+          <div className="col-3 align-items-center">Description</div>
+          <div className="col-2 align-items-center">Quantity</div>
+          <div className="col-3 align-items-center">Rate</div>
+          <div className="col-2 align-items-center">Total</div>
+        </div>
+        <form
+          method="post"
+          className="d-flex flex-column justify-content-evenly"
+        >
+          {invoiceObj.map((item, index) => {
+            return (
+              <NewItemRow
+                key={index}
+                index={index}
+                itemIndex={itemIndex}
+                setItemIndex={setItemIndex}
+                invoiceObj={invoiceObj}
+                setInvoiceObj={setInvoiceObj}
+              />
+            );
+          })}
+        </form>
+        <div className="d-flex flex-row justify-content-end bg-warning fw-bold">
+          <span className="d-flex col-10 justify-content-end">Total</span>
+          <span className="col-2 d-flex justify-content-end">£0.00</span>
+        </div>
+      </div>
+      <div>
+        {/*<CreateNewInvoiceButton quantity={quantity} rate={rate} description={description} total={total}/>*/}
+      </div>
+    </>
+  );
 }
 
-export default NewItemForm
+export default NewItemForm;
--- src/components/NewItemRow/index.jsx
+++ src/components/NewItemRow/index.jsx
@@ -1,46 +1,98 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
-function NewItemRow({index, setItemIndex, itemIndex, invoiceObj, setInvoiceObj}) {
+function NewItemRow({
+  index,
+  setItemIndex,
+  itemIndex,
+  invoiceObj,
+  setInvoiceObj,
+}) {
+  const [itemInfo, setItemInfo] = useState({
+    description: "",
+    quantity: 0,
+    rate: 0,
+    total: 0,
+  });
+  const rowTotal = (quantity, rate) => {
+    return quantity && rate
+      ? (Number(quantity) * Number(rate)).toFixed(2)
+      : (0).toFixed(2);
+  };
 
-    const [itemInfo, setItemInfo] = useState({description: '', quantity: 0, rate: 0, total: 0})
-    const rowTotal = (quantity, rate) => {
-        return quantity && rate ? (Number(quantity) * Number(rate)).toFixed(2) : (0).toFixed(2);
-    }
+  // setInvoiceObj([...invoiceObj, itemInfo]);
 
-    // setInvoiceObj([...invoiceObj, itemInfo]);
-
-    return (
-        <div className="d-flex flex-row w-100 small gap-1">
-            <div className="col-3"><textarea name="textarea" className="w-100" rows="4"
-                                             placeholder="Description" value={itemInfo['description']}
-                                             onChange={(e) => {
-                                                 {
-                                                     setItemInfo({...itemInfo, description: e.target.value})
-                                                 }
-                                             }}></textarea></div>
-            <div className="col-2"><input type="number" value={itemInfo['quantity']} className="w-100 p-1"
-                                          placeholder="Quantity" onChange={
-                (e) => {
-                    setItemInfo({...itemInfo, quantity: e.target.value})
-                }
-            }/></div>
-            <div className="col-3">
-                <div className="input-group input-medium">
-                    <span className="input-group-text h-25 p-1 bg-secondary-subtle">£</span>
-                    <input type="number" value={itemInfo['rate']} onChange={(e) => {
-                        setItemInfo({...itemInfo, rate: e.target.value})
-                    }} className="h-25 form-control input-lg p-1" readOnly="" placeholder="Rate"/>
-                </div>
-            </div>
-            <div className="col-2"><p>£{rowTotal(itemInfo.quantity, itemInfo.rate)}</p></div>
-            <div className="d-flex flex-column w-100 gap-2">
-                <span className="btn btn-success" onClick={() => {
-                    setItemIndex([...itemIndex, 1])
-                }}>+</span>
-                <span className="btn btn-danger">-</span>
-            </div>
+  return (
+    <div className="d-flex flex-row w-100 small gap-1">
+      <div className="col-3">
+        <textarea
+          name="textarea"
+          className="w-100"
+          rows="4"
+          placeholder="Description"
+          value={itemInfo["description"]}
+          onChange={(e) => {
+            {
+              setItemInfo({ ...itemInfo, description: e.target.value });
+              let newItemInfo = [...invoiceObj];
+              newItemInfo[index].description = e.target.value;
+              setInvoiceObj(newItemInfo);
+            }
+          }}
+        ></textarea>
+      </div>
+      <div className="col-2">
+        <input
+          type="number"
+          value={itemInfo["quantity"]}
+          className="w-100 p-1"
+          placeholder="Quantity"
+          onChange={(e) => {
+            setItemInfo({ ...itemInfo, quantity: e.target.value });
+            let newItemInfo = [...invoiceObj];
+            newItemInfo[index].quantity = e.target.value;
+            setInvoiceObj(newItemInfo);
+          }}
+        />
+      </div>
+      <div className="col-3">
+        <div className="input-group input-medium">
+          <span className="input-group-text h-25 p-1 bg-secondary-subtle">
+            £
+          </span>
+          <input
+            type="number"
+            value={itemInfo["rate"]}
+            onChange={(e) => {
+              setItemInfo({ ...itemInfo, rate: e.target.value });
+              let newItemInfo = [...invoiceObj];
+              newItemInfo[index].rate = e.target.value;
+              setInvoiceObj(newItemInfo);
+            }}
+            className="h-25 form-control input-lg p-1"
+            readOnly=""
+            placeholder="Rate"
+          />
         </div>
-    )
+      </div>
+      <div className="col-2">
+        <p>£{rowTotal(itemInfo.quantity, itemInfo.rate)}</p>
+      </div>
+      <div className="d-flex flex-column w-100 gap-2">
+        <span
+          className="btn btn-success"
+          onClick={() => {
+            setInvoiceObj([
+              ...invoiceObj,
+              { description: "", quantity: 0, rate: 0, total: 0 },
+            ]);
+          }}
+        >
+          +
+        </span>
+        <span className="btn btn-danger">-</span>
+      </div>
+    </div>
+  );
 }
 
-export default NewItemRow
+export default NewItemRow;
